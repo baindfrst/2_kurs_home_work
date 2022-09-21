2df80e0ded7a120380ebb0cc497864e0ee36d203
baindfrst <n11174@yandex.ru>
2022-09-21 18:09:49 +0300

    com

--- hw_04_10_1.c
+++ hw_04_10_1.c
@@ -3,30 +3,40 @@
 int count_of_one(int inp)
 {
     int count = 0;
-    for (int i = 0; i != 32; i ++)
+    for (int i = 0; i != 32; i++)
     {
-        count += ((inp << i) & 0x8000) >> 31;
+        count += (((inp << i) & 0x80000000) >> 31);
     }
     return count;
 }
 
-void sort(int* arr, int len)
+void print_in_double(int inp)
+{
+    for (int i = 0; i != 32; i++)
+    {
+        printf("%d", (((inp << i) & 0x80000000) >> 31));
+    }
+    printf(" ");
+}
+
+void sort(int* arr, int len) // сортировка методом простого обмена
 {
     for (int i = 0; i != len; i++)
     {
-        int count_max = 0;
+        int count_min = 32;
         int index_max = 0;
         int buf = 0;
         for (int j = i; j != len; j++)
         {
-            if (count_of_one(arr[j]) > count_max)
+            if ((count_of_one(arr[j]) < count_min) || ((count_of_one(arr[j]) == count_min) && (arr[j] > arr[index_max])))
             {
-                count_max = arr[j];
+                count_min = count_of_one(arr[j]);
                 index_max = j;
             }
         }
         buf = arr[i];
         arr[i] = arr[index_max];
+        arr[index_max] = buf;
     }
 }
 
@@ -39,6 +49,16 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
+    for (int i = 0; i != len; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+        for (int i = 0; i != len; i++)
+    {
+        print_in_double(arr[i]);
+    }
+    printf("\n");
     sort(arr, len);
     for (int i = 0; i != len; i++)
     {
